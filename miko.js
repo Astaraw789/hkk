ceaeda0706ae9cd09c7799b04781b981acdcac04
Astaraw789 <153795398+Astaraw789@users.noreply.github.com>
2024-01-04 04:03:47 +0200

    Update miko.js

--- miko.js
+++ miko.js
@@ -9,6 +9,6 @@ class Car {
   }
 }
 
-const myCar = new Car("Ford", 2014);
+const myCar = new Car("Ford", 20143);
 document.getElementById("demo").innerHTML =
 "My car is " + myCar.age() + " years old.";
